04028cd10858e452d8750b09ab8870907334c194
guilhermessena <guilherme.sena@tecnogroup.com.br>
2022-07-19 08:58:44 -0300

    refatorando o código, adicionando classes com extensão widgets

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,39 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
+      body: ListaTransferencia(),
       appBar: AppBar(
         title: Text('Transferências'),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {  },
+        onPressed: () {},
         child: Icon(Icons.add),
       ),
     ),
   ));
+}
+
+class ListaTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(
+      children: [
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.monetization_on),
+            title: Text('100.0'),
+            subtitle: Text('0001'),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.monetization_on),
+            title: Text('350.0'),
+            subtitle: Text('0101'),
+          ),
+        ),
+      ],
+    );
+  }
 }
